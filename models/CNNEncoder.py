e7b4c79bc632298f7da5f6df1e1eeb3369a7587d
kdrl <kdrl7@naver.com>
2017-09-20 16:15:17 +0900

    require gradで行列をstaticにする

--- models/CNNEncoder.py
+++ models/CNNEncoder.py
@@ -15,14 +15,14 @@ class CNNEncoder(nn.Module):
             self.embed.weight = xavier_normal(self.embed.weight)
         else:
             self.embed.weight.data.copy_(torch.from_numpy(word_embedding_array))
+        if self.config.embed_static:
+            self.word_embed.weight.requires_grad = False
         self.convs = nn.ModuleList([nn.Conv2d(1, self.config.cnn_output_channel, (n, self.config.word_edim)) for n in self.config.cnn_n_gram_list])
 
     def forward(self, x):
-        if self.config.embed_pad_zero:
+        if self.config.embed_static == False and self.config.embed_pad_zero == True:
             self.embed.weight.data[self.dictionary['word2idx']['<pad>']] = 0
         x = self.embed(x) # (batch_size,seq_len,word_edim)
-        if self.config.embed_static:
-            x = Variable(x)
         x = x.unsqueeze(1) # (batch_size,1,seq_len,word_edim)
         x = [F.relu(conv(x)).squeeze(3) for conv in self.convs] #[(batch_size,cnn_output_channel,seq_len), ...]*len(cnn_n_gram_list)
         x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x] #[(batch_size,cnn_output_channel), ...]*len(cnn_n_gram_list)
